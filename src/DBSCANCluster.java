0362ff057d7e611d3209c59135a476f86764b389
yuz1988 <yuz1988@iastate.edu>
2019-04-02 16:27:54 -0500

    Discussion on different cases of updateSeeds

--- src/DBSCANCluster.java
+++ src/DBSCANCluster.java
@@ -9,7 +9,7 @@ public class DBSCANCluster {
     private int clusterGlobalID; // cluster unique ID, start from 0
 
     public DBSCANCluster(final double eps, final int minPts) {
-        if (eps < 0.0d || minPts < 0) {
+        if (eps < 0.0d || minPts < 1) {
             throw new IllegalArgumentException("DBSCAN param cannot be negative");
         }
 
@@ -49,7 +49,9 @@ public class DBSCANCluster {
         List<Point> candidates = new ArrayList<>();
         final List<Point> neighbors = getNeighbors(newPoint, points);
         for (Point nbr : neighbors) {
-            if (nbr.epsNbrNum == (minPts - 1)) {
+            // increment the number of eps-neighbor
+            nbr.epsNbrNum++;
+            if (nbr.epsNbrNum == minPts) {
                 candidates.add(nbr);
             }
         }
@@ -58,19 +60,50 @@ public class DBSCANCluster {
         for (Point q_Prime : candidates) {
             List<Point> q_Prime_Neighbors = getNeighbors(q_Prime, points);
             for (Point q : q_Prime_Neighbors) {
-                int numEpsNbrs = q.epsNbrNum;
-                if (q.euclidDist(newPoint) <= eps) {
-                    numEpsNbrs++;
-                }
-                if (numEpsNbrs >= minPts) {
+                if (q.epsNbrNum >= minPts) {
                     updateSeed.add(q);
                 }
             }
         }
 
+        points.add(newPoint);
+
         // Different cases based on the updateSeed
         if (updateSeed.isEmpty()) {
+            newPoint.clusterIndex = Point.NOISE;
+        } else {
+            // set contains only non-noise cluster index.
+            HashSet<Integer> set = new HashSet<>();
+            for (Point seed : updateSeed) {
+                if (seed.clusterIndex != -1) {
+                    set.add(seed.clusterIndex);
+                }
+            }
 
+            if (set.isEmpty()) {
+                // case 1: all seeds were noise
+                for (Point seed : updateSeed) {
+                    expandSeedToCluster(seed, points, clusterGlobalID);
+                    clusterGlobalID++;
+                }
+                System.out.println("Create a new cluster from new point\n");
+            } else if (set.size() == 1) {
+                // retrieve the unique cluster id.
+                int uniqueClusterID = -1;
+                for (int id : set) {
+                    uniqueClusterID = id;
+                }
+                // case 2: seeds contain core point of exactly one cluster C
+                for (Point seed : updateSeed) {
+                    expandSeedToCluster(seed, points, uniqueClusterID);
+                }
+                System.out.println("Absorb to cluster " + uniqueClusterID +
+                        "\n");
+            } else {
+                // case 3: seeds contains several clusters, merge them
+                mergeClusters(updateSeed, points, set);
+                System.out.println("Merge clusters \n");
+            }
         }
     }
 
@@ -119,12 +152,13 @@ public class DBSCANCluster {
      *
      * @param point  the point to look for
      * @param points all points
-     * @return neighbors not including point itself
+     * @return neighbors including point itself
      */
     private List<Point> getNeighbors(final Point point, final List<Point> points) {
         final List<Point> neighbors = new ArrayList<>();
         for (final Point neighbor : points) {
-            if (point != neighbor && point.euclidDist(neighbor) <= eps) {
+            // include point itself
+            if (point.euclidDist(neighbor) <= eps) {
                 neighbors.add(neighbor);
             }
         }
